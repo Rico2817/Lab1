fd08a822cd28d6f6a4f08c8ace8d53ce4c919fde
Rico2817 <1434834580@qq.com>
2021-04-15 17:14:48 +0800

    HW-week6 commit

--- src/main/java/com/LiQijun/week6/demo/JDBCServletContextListenner.java
+++ src/main/java/com/LiQijun/week6/demo/JDBCServletContextListenner.java
@@ -1,2 +1,44 @@
-package com.LiQijun.week6.demo;public class JDBCServletContextListenner {
+package com.LiQijun.week6.demo;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+@WebListener
+public class JDBCServletContextListenner implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        ServletContext context= sce.getServletContext();
+
+        String url ="jdbc:sqlserver://127.0.0.1:1433;databaseName=userdb;";
+        String user="sa";
+        String password="admin123456";                          //数据库 URL
+        try{
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            System.out.println("SqlServer数据库驱动连接成功！");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        try{
+            Connection con= DriverManager.getConnection(url,user,password);
+            System.out.println("SqlServer数据库连接成功！"+con);
+            System.out.println("i am in contextInitialized");
+            context.setAttribute("con",con);
+        }catch (Exception e){
+            String[] infos = { "未能成功连接数据库！", "请确认本软件是否已经运行！" };
+            JOptionPane.showMessageDialog(null, infos);
+            System.exit(0);
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("i am in contextDestroyed");
+        sce.getServletContext().removeAttribute("con");
+    }
 }
--- src/main/webapp/userInfo.jsp
+++ src/main/webapp/userInfo.jsp
@@ -6,11 +6,15 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
-</html>
+<%@include file="header.jsp"%>
+<h1>
+    User Info
+</h1>
+<table>
+    <tr><td>username:</td><td><%=request.getAttribute("username")%></td></tr>
+    <tr><td>password:</td><td><%=request.getAttribute("password")%></td></tr>
+    <tr><td>email:</td><td><%=request.getAttribute("email")%></td></tr>
+    <tr><td>gender:</td><td><%=request.getAttribute("gender")%></td></tr>
+    <tr><td>birthDate:</td><td><%=request.getAttribute("birthDate")%></td></tr>
+</table>
+<%@include file="footer.jsp"%>
